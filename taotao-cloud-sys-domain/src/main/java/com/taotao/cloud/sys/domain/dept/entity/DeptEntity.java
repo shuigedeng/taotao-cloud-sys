a9fb7fd32f45044b01e448a2dcafa97b520158a1
dengtao <981376577@qq.com>
2025-01-21 18:43:13 +0800

    xx

--- taotao-cloud-sys-domain/src/main/java/com/taotao/cloud/sys/domain/dept/entity/DeptEntity.java
+++ taotao-cloud-sys-domain/src/main/java/com/taotao/cloud/sys/domain/dept/entity/DeptEntity.java
@@ -9,8 +9,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Accessors(fluent = true)
 @Schema(name = "Dept", description = "部门")
