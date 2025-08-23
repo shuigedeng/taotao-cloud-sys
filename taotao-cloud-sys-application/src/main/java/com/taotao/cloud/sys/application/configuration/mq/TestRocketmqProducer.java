1c8960975b7a757b9dd859b6df2ed03cc3293b13
shuigedeng <981376577@qq.com>
2025-08-23 15:21:22 +0800

    开启2025.09

--- taotao-cloud-sys-application/src/main/java/com/taotao/cloud/sys/application/configuration/mq/TestRocketmqProducer.java
+++ taotao-cloud-sys-application/src/main/java/com/taotao/cloud/sys/application/configuration/mq/TestRocketmqProducer.java
@@ -1,33 +1,33 @@
-//package com.taotao.cloud.sys.application.configuration.mq;
-//
-//import com.alibaba.fastjson2.JSON;
-//import com.taotao.boot.common.utils.log.LogUtils;
-//import java.util.HashMap;
-//import java.util.Map;
-//import java.util.UUID;
-//import org.apache.rocketmq.client.producer.SendResult;
-//import org.apache.rocketmq.spring.core.RocketMQTemplate;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class TestRocketmqProducer implements CommandLineRunner {
-//
-//	@Autowired
-//	private RocketMQTemplate rocketMQTemplate;
-//
-//	@Override
-//	public void run(String... args) throws Exception {
-//		System.out.println("应用启动完成，执行初始化操作...");
-//		Map<String, Object> body = new HashMap<>();
-//		body.put("uuid", UUID.randomUUID().toString());
-//		try {
-//			SendResult sendResult = rocketMQTemplate.syncSend("taotao-cloud-sys", JSON.toJSONString(body));
-//			LogUtils.info("send msg success, topic={}, sendResult={}", "taotao-cloud-sys", sendResult);
-//		} catch (Exception e) {
-//			LogUtils.error("send msg error, topic={}, errorMsg={}", "taotao-cloud-sys", e.getMessage(), e);
-//		}
-//
-//	}
-//}
+package com.taotao.cloud.sys.application.configuration.mq;
+
+import com.alibaba.fastjson2.JSON;
+import com.taotao.boot.common.utils.log.LogUtils;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestRocketmqProducer implements CommandLineRunner {
+
+	@Autowired
+	private RocketMQTemplate rocketMQTemplate;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("应用启动完成，执行初始化操作...");
+		Map<String, Object> body = new HashMap<>();
+		body.put("uuid", UUID.randomUUID().toString());
+		try {
+			SendResult sendResult = rocketMQTemplate.syncSend("taotao-cloud-sys", JSON.toJSONString(body));
+			LogUtils.info("send msg success, topic={}, sendResult={}", "taotao-cloud-sys", sendResult);
+		} catch (Exception e) {
+			LogUtils.error("send msg error, topic={}, errorMsg={}", "taotao-cloud-sys", e.getMessage(), e);
+		}
+
+	}
+}
