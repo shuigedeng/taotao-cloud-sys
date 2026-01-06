2145e6d9b7f5c57811214aaa2cf75bac1eb312d5
shuigedeng <981376577@qq.com>
2026-01-06 17:57:45 +0800

    开启2026.01

---
+++ taotao-cloud-sys-api/src/main/java/com/taotao/cloud/sys/api/rpc/dto/callback/DictCallback.java
@@ -0,0 +1,10 @@
+package com.taotao.cloud.sys.api.rpc.dto.callback;
+
+/**
+ * DictCallback
+ *
+ * @author shuigedeng
+ * @version 2026.02
+ * @since 2025-12-19 09:30:45
+ */
+public class DictCallback {}
